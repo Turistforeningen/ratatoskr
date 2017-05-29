f84f21f9b1639680f684495d34fa88528176b261
Roar Skaar <roar.skaar@gmail.com>
2017-05-29 18:59:54 +0200

    feat(version): return promise that resolves when version file is read

--- server/version.js
+++ server/version.js
@@ -5,13 +5,17 @@ const fs = require('fs');
 
 const version = {tag: 'unknown'};
 
-fs.readFile(`${__dirname}/version-tag`, {encoding: 'utf-8'}, (err, data) => {
-  if (err) {
-    console.log('UNABLE TO READ VERSION FILE'); // eslint-disable-line
-    console.log(err); // eslint-disable-line
-  } else {
-    version.tag = data.trim();
-  }
+version.promise = new Promise((resolve, reject) => {
+  fs.readFile(`${__dirname}/version-tag`, {encoding: 'utf-8'}, (err, data) => {
+    if (err) {
+      console.log('UNABLE TO READ VERSION FILE'); // eslint-disable-line
+      console.log(err); // eslint-disable-line
+      reject();
+    } else {
+      version.tag = data.trim();
+      resolve(version.tag);
+    }
+  });
 });
 
 
