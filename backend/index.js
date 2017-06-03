2b1e12fda5bf032aaee7556f6d8103bcc6a6d83e
Roar Skaar <roar.skaar@gmail.com>
2017-06-03 06:09:23 +0200

    fix(backend): add special assets route to sw.js

--- backend/index.js
+++ backend/index.js
@@ -39,6 +39,7 @@ if (environment.production) {
 // Assets are built through Webpack and will be loaded using webpack dev server
 // when in development mode
 app.use('/assets', express.static('/ratatoskr/build/assets'));
+app.use('/sw.js', express.static('/ratatoskr/build/sw.js'));
 
 
 // Configure nunjucks template engine
