79106ff6d5713d6d6477d241c1191d2bcea51997
Roar Skaar <roar.skaar@gmail.com>
2017-06-03 04:56:42 +0200

    refactor(backend): tidy up routes for appcache manifest

--- backend/controllers/appcache.js
+++ backend/controllers/appcache.js
@@ -10,35 +10,32 @@ const router = new Router();
 
 
 // Service Worker Manifest
-router.get(
-  ['/manifest.html', '/appcache/manifest.html'],
-  (req, res, next) => {
-    if (environment.production) {
-      res.sendFile('/ratatoskr/build/appcache/manifest.html');
-    }
-
-    fetch('http://assets.medlem.dnt.local/appcache/manifest.html')
-      .then((response) => response.text())
-      .then((text) => {
-        res.send(text);
-      });
+router.get('/manifest.html', (req, res, next) => {
+  if (environment.production) {
+    res.sendFile('/ratatoskr/build/appcache/manifest.html');
   }
-);
 
-router.get(
-  ['/manifest.appcache', '/appcache/manifest.appcache'],
-  (req, res, next) => {
-    if (environment.production) {
-      res.sendFile('/ratatoskr/build/appcache/manifest.appcache');
-    }
+  // Get manifest from webpack output in dev mode
+  fetch('http://assets.medlem.dnt.local/appcache/manifest.html')
+    .then((response) => response.text())
+    .then((text) => {
+      res.send(text);
+    });
+});
 
-    fetch('http://assets.medlem.dnt.local/appcache/manifest.appcache')
-      .then((response) => response.text())
-      .then((text) => {
-        res.send(text);
-      });
+
+router.get('/manifest.appcache', (req, res, next) => {
+  if (environment.production) {
+    res.sendFile('/ratatoskr/build/appcache/manifest.appcache');
   }
-);
+
+  // Get manifest from webpack output in dev mode
+  fetch('http://assets.medlem.dnt.local/appcache/manifest.appcache')
+    .then((response) => response.text())
+    .then((text) => {
+      res.send(text);
+    });
+});
 
 
 module.exports = router;
--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -10,9 +10,6 @@ const apiController = require('./api');
 const router = new Router();
 
 
-// Service Worker Manifest
-router.use('/', appcacheController);
-
 // User authenticated - Return React app
 router.get('/', (req, res, next) => {
   if (req.user) {
@@ -23,12 +20,14 @@ router.get('/', (req, res, next) => {
   }
 });
 
+
 // User not authenticated - Return splash page
 router.get('/', (req, res, next) => {
   res.render('splash.html');
 });
 
 // Add controllers
+router.use('/appcache', appcacheController);
 router.use('/login', authController);
 router.use('/api', apiController);
 
