80cc9d70aec4ab4db070cb2ed055f8df65e115b8
Roar Skaar <roar.skaar@gmail.com>
2017-09-05 08:34:13 +0200

    feat(app): add last updated timestamp in footer

--- frontend/js/components/App/Footer.jsx
+++ frontend/js/components/App/Footer.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { autobind } from 'core-decorators';
 
+import { getLastUpdated } from '../../selectors/user/data';
 import { getVersion } from '../../selectors/version';
 
 import Logout from './Logout.jsx';
@@ -9,6 +10,23 @@ import ExternalA from '../common/ExternalA.jsx';
 
 
 class Footer extends Component {
+  @autobind
+  renderLastUpdate() {
+    const { lastUpdated } = this.props;
+    const pad = '00';
+    const ddStr = lastUpdated.getDate().toString();
+    const DD = pad.substring(0, 2 - ddStr.length) + ddStr;
+    const mmStr = lastUpdated.getMonth().toString();
+    const MM = pad.substring(0, 2 - mmStr.length) + mmStr;
+    const YYYY = lastUpdated.getFullYear();
+    const hhStr = lastUpdated.getHours().toString();
+    const HH = pad.substring(0, 2 - hhStr.length) + hhStr;
+    const iiStr = lastUpdated.getMinutes().toString();
+    const II = pad.substring(0, 2 - iiStr.length) + iiStr;
+
+    return `${DD}.${MM}.${YYYY} ${HH}:${II}`;
+  }
+
   render() {
     const { version } = this.props;
     const year = new Date().getFullYear();
@@ -18,6 +36,11 @@ class Footer extends Component {
         <footer>
           <nav>
             <Logout />
+
+            <div className="updated-timestamp">
+              Medlemsdetaljer sist oppdatert: {this.renderLastUpdate()}
+            </div>
+
             <ul className="footer-menu footer-menu--sub">
               <li className="footer-menu__item">
                 <ExternalA href="https://www.dnt.no/medlem/">
@@ -53,6 +76,7 @@ class Footer extends Component {
 
 const mapStateToProps = (state) => ({
   version: getVersion(state),
+  lastUpdated: getLastUpdated(state),
 });
 
 
