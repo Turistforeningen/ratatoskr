68eabd9e3af002ec0986c386840bec7dbb81d972
Roar Skaar <roar.skaar@gmail.com>
2017-10-24 14:06:28 +0200

    fix(app): ignore if Raven is not present

--- frontend/js/lib/raven.js
+++ frontend/js/lib/raven.js
@@ -2,10 +2,14 @@
 
 export default () => {
   if (process.env.NODE_ENV === 'production') {
-    Raven
-      .config('https://979cb1ce6a43473290a6212f6bfc004c@sentry.io/178949', {
-        release: ratatoskr.version,
-      })
-      .install();
+    try {
+      Raven
+        .config('https://979cb1ce6a43473290a6212f6bfc004c@sentry.io/178949', {
+          release: ratatoskr.version,
+        })
+        .install();
+    } catch (error) {
+      // Ignore if Raven is not there (for instance in offline mode)
+    }
   }
 };
