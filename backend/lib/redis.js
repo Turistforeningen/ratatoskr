6d10b5a675c8e8daca9a7e693ba290fa9fdee362
Roar Skaar <roar.skaar@gmail.com>
2018-04-04 11:43:54 +0200

    chore(redis): enable reading redis hostname from env variable

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -2,5 +2,8 @@
 
 const Redis = require('ioredis');
 
+const redisHostname = process.env.REDIS_HOSTNAME
+  ? process.env.REDIS_HOSTNAME
+  : 'redis';
 
-module.exports = new Redis(6379, 'redis');
+module.exports = new Redis(6379, redisHostname);
